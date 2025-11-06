a40fd8e6dd1b5f173c2441d457ecfdafb19d1090
Ashani <ashanitharukshi2002@gmail.com>
2025-11-06 20:43:17 +0530

    Enhance Header component styling by updating font weight for improved emphasis

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div className='p-5 flex justify-between items-center border shadow-md'>
         <div className='flex items-center'><Image src="/logo.svg" alt="Logo" width={40} height={20}  />
-    <span className='text-2xl text-bold text-green-500'>Spender</span></div>
+    <span className='text-2xl font-extrabold text-green-500'>Spender</span></div>
     <Button>Get Started</Button>
     </div>
   )
