bfe4b97d2fbc1e88439fb8074865e0ca57b222d8
Ashani <ashanitharukshi2002@gmail.com>
2025-11-06 23:32:13 +0530

    Refactor SignIn page layout to include Header component for improved structure and user experience

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,13 @@
+import Header from '@/app/_components/Header'
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+  return(
+    <>
+      <Header />
+      <div className='items-center justify-center flex min-h-screen'>
+          <SignIn />
+      </div>
+    </>
+  )
 }
