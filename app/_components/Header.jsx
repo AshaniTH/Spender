0d5c4feb85e586d97f4d30e760c08a047dd46cc6
Ashani <ashanitharukshi2002@gmail.com>
2025-11-06 16:51:10 +0530

    Refactor Header component layout to include button and improve styling

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 function Header() {
   return (
-    <div className='p-5 flex items-center'><Image src="/logo.svg" alt="Logo" width={40} height={20}  />
+    <div className='p-5 flex justify-between items-center border shadow-md'>
+        <div className='flex items-center'><Image src="/logo.svg" alt="Logo" width={40} height={20}  />
     <span className='text-2xl text-bold text-green-500'>Spender</span></div>
+    <Button>Get Started</Button>
+    </div>
   )
 }
 
