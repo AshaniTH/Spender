81597f61bc86fc402451a9c218e152125dd53c74
Ashani <ashanitharukshi2002@gmail.com>
2025-11-06 23:32:07 +0530

    Add UserButton component to Dashboard for user authentication

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Dashbord() {
   return (
-    <div>Dashbord</div>
+    <div>
+        <UserButton />
+    </div>
   )
 }
 
