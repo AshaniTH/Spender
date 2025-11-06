f37c694a5da11fb3177df602c4a997b0ff8cdadf
Ashani <ashanitharukshi2002@gmail.com>
2025-11-06 23:38:26 +0530

    Refactor Header component to display UserButton for signed-in users, enhancing user experience

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,18 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import { useUser, UserButton } from '@clerk/nextjs';
 
 function Header() {
+    const {user,isSignedIn} = useUser();
   return (
     <div className='p-5 flex justify-between items-center border shadow-md'>
         <div className='flex items-center'><Image src="/logo.svg" alt="Logo" width={40} height={20}  />
     <span className='text-2xl font-extrabold text-green-500'>Spender</span></div>
-    <Button>Get Started</Button>
+    {isSignedIn? 
+    <UserButton /> :
+    <Button>Get Started</Button>}
     </div>
   )
 }
