d138089ccab9581abe96ef1ca12a6e9f3ec457b7
Ashani <ashanitharukshi2002@gmail.com>
2025-11-07 16:50:06 +0530

    Add DashboardHeader component and integrate it into DashboardLayout for improved layout structure

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { LayoutDashboardIcon, PiggyBank, ReceiptText, ShieldCheck, User  } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 function SideNav() {
   const menuList = [
@@ -9,45 +12,57 @@ function SideNav() {
       id: 1,
       name: 'Dashboard',
       icon: LayoutDashboardIcon,
+      path: '/dashboard'
     },
     {
       id:2,
       name:'Budgets',
       icon: PiggyBank,
+      path:'/dashboard/budgets'
     },
     {
       id:3,
       name:'Expenses',
       icon: ReceiptText,
+      path:'/dashboard/expenses'
     },
     {
       id:4,
       name:'Upgrade',
       icon: ShieldCheck,
+      path:'/dashboard/upgrade'
     }
 
   ]
+
+  const pathname = usePathname();
+  useEffect(()=>{
+    console.log("Current Path:", pathname);
+  },[pathname])
   return (
-
-    
-
-
-    <div className='h-screen border shadow-sm'>
+   <div className='h-screen border shadow-sm'>
        
               <div className='p-5 flex items-center'><Image src="/logo.svg" alt="Logo" width={40} height={20}  />
           <span className='text-2xl font-extrabold text-green-500'>Spender</span>
           </div>
 
           {/* Menu List */}
-          <div>
-            {menuList.map((menu,Index)=>(
-              <h2 className='flex gap-2 items-center text-gray-500 font-medium p-5 cursor-pointer rounded-md hover:text-green-700 hover:bg-green-200 '>
-                <menu.icon/>
-                {menu.name}
-              </h2>
-            ))}
-            
+          <div className='mt-5'>
+            {menuList.map((menu) => {
+              const Icon = menu.icon
+              const isActive = pathname === menu.path
+              return (
+                <Link href={menu.path} key={menu.id}>
+                  <h2 className={`flex gap-2 items-center text-gray-500 font-medium mb-2 p-5 cursor-pointer rounded-md hover:text-green-700 hover:bg-green-200 ${isActive ? 'text-green-700 bg-green-200' : ''}`}>
+                    <Icon />
+                    {menu.name}
+                  </h2>
+                </Link>
+              )
+            })}
+
           </div>
+
           <div className='fixed bottom-10 p-5 flex gap-2 items-center text-gray-500 font-medium'>
             <UserButton/>
             Profile
