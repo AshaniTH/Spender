3f7e86c96cec0e656dec881908b1b9d4b664812a
Ashani <ashanitharukshi2002@gmail.com>
2025-11-07 12:11:15 +0530

    Enhance SideNav styling with border and shadow for improved visual hierarchy

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function SideNav() {
   return (
-    <div className='h-screen'>
+    <div className='h-screen border shadow-sm'>
        
               <div className='p-5 flex items-center'><Image src="/logo.svg" alt="Logo" width={40} height={20}  />
           <span className='text-2xl font-extrabold text-green-500'>Spender</span>
--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -4,7 +4,7 @@ import SideNav from './_components/SideNav'
 function DashboardLayout({children}) {
   return (
     <div>
-        <div className='fixed md:w-64 hidden md:block bg-red-400'>
+        <div className='fixed md:w-64 hidden md:block'>
             <SideNav />
         </div>
         <div className='md:ml-64 bg-green-200'>{children}</div>
