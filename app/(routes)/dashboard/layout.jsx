91d2d22b9dd006dbda7b2d872f51bf08c95ed108
Ashani <ashanitharukshi2002@gmail.com>
2025-11-09 00:50:10 +0530

    Add user budget check in DashboardLayout for improved user experience

--- app/(routes)/dashboard/layout.jsx
+++ app/(routes)/dashboard/layout.jsx
@@ -1,8 +1,28 @@
+"use client"
 import React from 'react'
 import SideNav from './_components/SideNav'
 import DashboardHeader from './_components/DashboardHeader'
+import { db } from '@/utils/dbConfig'
+import { budgets } from '@/utils/schema'
+import { eq } from 'drizzle-orm'
 
 function DashboardLayout({children}) {
+
+  const {user} = useUser();
+
+  useEffect(()=>{
+    user&&checkUserBudgets();
+  },[user])
+const checkUserBudgets = async () => {
+  const result = await db.select().from(budgets)
+  .where(eq(budgets.created_by,user?.primaryEmailAddress?.emailAddress))
+
+  console.log(result);
+  if(result ?.length==0){
+    router.replace('/dashboard/budgets')
+  }
+}
+
   return (
     <div>
         <div className='fixed md:w-64 hidden md:block'>
