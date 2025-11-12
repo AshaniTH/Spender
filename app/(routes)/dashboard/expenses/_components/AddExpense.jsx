066c9fdfca9a388bbd32021623c77f6b048cb0ba
Ashani <ashanitharukshi2002@gmail.com>
2025-11-12 23:31:47 +0530

    feat: add AddExpense component for adding new expenses

---
+++ app/(routes)/dashboard/expenses/_components/AddExpense.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+function AddExpense() {
+  return (
+    <div className="mt-4">
+      <Button variant="outline">Add Expense</Button>
+    </div>
+  )
+}
+
+export default AddExpense
