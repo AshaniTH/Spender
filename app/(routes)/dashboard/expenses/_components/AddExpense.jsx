84a172ece48421bac9d7e7a0b97c58238d16f34c
Ashani <ashanitharukshi2002@gmail.com>
2025-11-13 21:35:38 +0530

    feat: add input field for expense name in AddExpense component

--- app/(routes)/dashboard/expenses/_components/AddExpense.jsx
+++ app/(routes)/dashboard/expenses/_components/AddExpense.jsx
@@ -1,11 +1,16 @@
 "use client"
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 function AddExpense() {
   return (
     <div className="mt-4">
       <h2 className="font-bold text-lg">Add Expense</h2>
+      <div className='mt-2'>
+        <h2 className='text-black font-medium my-1'></h2>
+        <Input placeholder='Expense Name' className='w-full mb-2' />
+      </div>
     </div>
   )
 }
