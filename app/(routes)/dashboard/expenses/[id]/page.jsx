4939355141ab4f6f67f773f0b6352a6c14f4ab29
Ashani <ashanitharukshi2002@gmail.com>
2025-11-14 21:26:04 +0530

    fix: update layout structure in ExpensesScreen for better spacing and alignment

--- app/(routes)/dashboard/expenses/[id]/page.jsx
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -53,13 +53,15 @@ function ExpensesScreen() {
   if (!budget) return <div className="p-4">No budget selected</div>
 
   return (
-    <div className="p-10">
-      <div className="flex items-center justify-between">
+    <div className="p-10 gap-3 space-y-5">
+      
         <h2 className="text-2xl font-semibold">My Expenses</h2>
+        <div className="flex  items-center justify-between">
         <AddExpense />
-      </div>
-      <div>
+      
+  <div>
         <BudgetItem budget={budget} />
+      </div>
       </div>
      
     </div>
