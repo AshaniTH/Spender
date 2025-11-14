a307fbd04ec8cb6f93ca110834b01ee6ff6155d9
Ashani <ashanitharukshi2002@gmail.com>
2025-11-14 21:08:53 +0530

    feat: implement state management for expense name and amount in AddExpense component

--- app/(routes)/dashboard/expenses/_components/AddExpense.jsx
+++ app/(routes)/dashboard/expenses/_components/AddExpense.jsx
@@ -1,27 +1,31 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
 function AddExpense() {
+
+  const [name, setName] = useState();
+  const [amount, setAmount] = useState();
+
   return (
     <div className="mt-4">
       <h2 className="font-bold text-lg">Add Expense</h2>
       <div className='mt-2'>
         <h2 className='text-black font-medium my-1'></h2>
         <Input placeholder='Expense Name' className='w-full mb-2' 
-        onChange={(e) => setInCacheMap(e.target.value)}
+        onChange={(e) => setName(e.target.value)}
         />
 
       </div>
       <div className='mt-2'>
         <h2 className='text-black font-medium my-1'></h2>
-        <Input placeholder='Expense Name' className='w-full mb-2' 
-        onChange={(e) => setInCacheMap(e.target.value)}
+        <Input placeholder='Expense Amount' className='w-full mb-2' 
+        onChange={(e) => setAmount(e.target.value)}
         />
 
       </div>
-      <Button className='mt-2'>Add Expense</Button>
+      <Button disabled={!(name && amount)} className='mt-2'>Add Expense</Button>
 
     </div>
   )
